fix: ball pos(offset & neg), yaw(-init) refactor: return robot_obs with L1 and L2 button, instead of directly stopping the robot when L2 pressed refactor: yaw computation chore: instantiate robot and ball detector outside env

deploy.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import torch
 import zmq
 
-from math_utils import project_gravity, wrap_to_pi_
+from math_utils import project_gravity, wrap_to_pi
 from robot import Robot, RobotObservation
 
 
@@ -22,22 +22,35 @@ class BallDetector:
         except zmq.error.Again:
             return
 
-        if score > 0.5:
-            self.box_corner = box_corner
+        self.box_corner = box_corner if score > 0.7 else None
 
     def get_ball_pos(self):
         if self.box_corner is None:
             return torch.tensor([0.2, 0.0, 0.0], dtype=torch.float32)
         x0, y0, x1, y1 = self.box_corner
         image_width = 480
-        offset = (x0 + x1) / 2 - image_width / 2
+        offset_from_center = (x0 + x1) / 2 - image_width / 2
+        # the line y = 0 (the x-axis) has offset_from_center ≈ -40
+        offset_from_x_axis = offset_from_center + 40
+
         w = x1 - x0
         h = y1 - y0
         size = math.sqrt(w * h)
 
-        r = 20 / size  # ball at 1m distance has size 20px
-        θ = offset * 0.3 * (math.pi / 180)  # 0.3 deg per pixel
-        return torch.tensor([r * math.cos(θ), r * math.sin(θ), 0.0], dtype=torch.float32)
+        # ball at 1m distance has size 20px
+        r = 20 / size
+
+        # the positive direction of offset points to the right
+        # the positive direction of θ is counter-clockwise
+        # the ray θ = 0 points forward
+
+        θ = -math.radians(0.4 * offset_from_x_axis)  # 0.4° per pixel
+
+        # right-handed coordinate system
+        # the positive x-axis points forward
+        # the positive y-axis points to the left
+        # the positive z-axis points up
+        return torch.tensor([r * math.cos(θ), r * math.sin(θ), 0], dtype=torch.float32)
 
 
 def load_policy(root: Path):
@@ -67,8 +80,8 @@ class DribbleEnv(RealtimeEnv):
 
     # gait type parameters
     phase = 0.5
-    offset = 0
-    bound = 0
+    offset = 0.0
+    bound = 0.0
 
     foot_gait_offsets = torch.tensor([phase + offset + bound, offset, bound, phase], dtype=torch.float32)
 
@@ -82,7 +95,7 @@ class DribbleEnv(RealtimeEnv):
     action_scale = 0.25
     hip_scale_reduction = torch.tensor([0.5, 1, 1] * 4, dtype=torch.float32)
 
-    def __init__(self, history_len: int):
+    def __init__(self, history_len: int, robot: Robot, ball_detector: BallDetector):
         assert history_len > 0
 
         self.history_len = history_len
@@ -94,15 +107,17 @@ class DribbleEnv(RealtimeEnv):
 
         self.gait_index = torch.zeros(1, dtype=torch.float32)
 
-        self.robot = Robot()
-        self.ball_detector = BallDetector()
+        self.yaw_init = 0.0
+
+        self.robot = robot
+        self.ball_detector = ball_detector
 
     def observe(self):
         self.ball_detector.refresh()
         robot_obs = self.robot.get_obs()
         obs = self.make_obs(robot_obs)
         self.store_obs(obs)
-        return self.buffer[self.t - self.history_len:self.t]
+        return self.buffer[self.t - self.history_len:self.t], robot_obs
 
     def advance(self, action):
         self.action_t_minus1[:] = self.action_t
@@ -147,7 +162,7 @@ class DribbleEnv(RealtimeEnv):
         action = self.action_t
         last_action = self.action_t_minus1
         clock = torch.sin(2 * torch.pi * self.foot_indices())
-        yaw = wrap_to_pi_(robot_obs.rpy[None, 2].clone())
+        yaw = torch.tensor([wrap_to_pi(robot_obs.yaw - self.yaw_init)], dtype=torch.float32)
         timing = self.gait_index
 
         return torch.cat([
@@ -164,4 +179,38 @@ class DribbleEnv(RealtimeEnv):
     def foot_indices(self):
         return self.gait_index + self.foot_gait_offsets
 
+    def set_yaw_init(self, yaw_init: float):
+        self.yaw_init = yaw_init
 
+
+def main():
+    import time
+    policy = load_policy(Path(__file__).resolve().parent)
+    robot = Robot()
+    ball_detector = BallDetector()
+    env = DribbleEnv(history_len=15, robot=robot, ball_detector=ball_detector)
+
+    robot.init()
+    while True:
+        obs, robot_obs = env.observe()
+        env.advance(torch.zeros(12, dtype=torch.float32))
+        if robot_obs.L1:
+            break
+        time.sleep(0.02)
+
+    env.set_yaw_init(robot_obs.yaw)
+
+    while True:
+        obs, robot_obs = env.observe()
+        action = policy(obs)
+        env.advance(action)
+        if robot_obs.L2:
+            break
+        time.sleep(0.02)
+
+    robot.stopped.set()
+    robot.background_thread.join()
+
+
+if __name__ == '__main__':
+    main()

math_utils.py
@@ -1,28 +1,35 @@
+import math
+
 import torch
 
-def quaternion_multiply(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
+
+def quaternion_multiply(q1: torch.Tensor, q2: torch.Tensor):
     w1, u1 = q1[..., :1], q1[..., 1:]
     w2, u2 = q2[..., :1], q2[..., 1:]
 
     scalar = w1 * w2 - torch.einsum('...i,...i', u1, u2).unsqueeze(-1)
     vector = w1 * u2 + w2 * u1 + torch.cross(u1, u2, dim=-1)
-    
+
     return torch.cat([scalar, vector], dim=-1)
 
-def quaternion_conjugate(q: torch.Tensor) -> torch.Tensor:
+
+def quaternion_conjugate(q: torch.Tensor):
     w, u = q[..., :1], q[..., 1:]
     return torch.cat([w, -u], dim=-1)
 
-def rotate_vector_by_quaternion(v: torch.Tensor, q: torch.Tensor) -> torch.Tensor:
+
+def rotate_vector_by_quaternion(v: torch.Tensor, q: torch.Tensor):
     v_quaternion = torch.cat([v.new_zeros(*v.shape[:-1], 1), v], dim=-1)
     rotated_quaternion = quaternion_multiply(q, quaternion_multiply(v_quaternion, quaternion_conjugate(q)))
     return rotated_quaternion[..., 1:]
 
-def project_gravity(quaternion: torch.Tensor) -> torch.Tensor:
+
+def project_gravity(quaternion: torch.Tensor):
     gravity = torch.tensor([0.0, 0.0, -1], dtype=torch.float32)
     return rotate_vector_by_quaternion(gravity, quaternion_conjugate(quaternion))
 
-def wrap_to_pi_(x: torch.Tensor):
-    x.remainder_(2 * torch.pi)
-    x[x > torch.pi] -= 2 * torch.pi
-    return x
+
+def wrap_to_pi(x: float):
+    # wrap ℝ to [-π, π] while preserving cos(x) and sin(x)
+    # (x ± math.pi) % (2 * math.pi) ± math.pi
+    return math.atan2(math.sin(x), math.cos(x))

robot.py
@@ -79,11 +79,15 @@ class RobotObservation:
     joint_velocity: torch.Tensor
     gyroscope: torch.Tensor
     quaternion: torch.Tensor
-    rpy: torch.Tensor
+    roll: float
+    pitch: float
+    yaw: float
     lx: float
     ly: float
     rx: float
     ry: float
+    L1: bool
+    L2: bool
 
 
 class Robot:
@@ -202,7 +206,7 @@ class Robot:
         imu = self.imu
         gyroscope = torch.tensor(imu.gyroscope, dtype=dtype)  # rpy order, rad/s
         quaternion = torch.tensor(imu.quaternion, dtype=dtype)  # (w, x, y, z) order, normalized
-        rpy = torch.tensor(imu.rpy, dtype=dtype)  # rpy order, rad
+        roll, pitch, yaw = imu.rpy  # rpy order, rad
 
         rc = self.state.wirelessRemote  # std::array<uint8_t, 40> => list[int]
         # stdlib struct.unpack is faster than convert tensor then .view(torch.float32)
@@ -212,20 +216,23 @@ class Robot:
         # LSB -> MSB
         # rc[2] = [R1, L1, start, select][R2, L2, F1, F2]
         # rc[3] = [A, B, X, Y][up, right, down, left]
-        L2 = rc[2] & 0b0010_0000
-        if L2 != 0:
-            self.stopped.set()
+        button_L1 = rc[2] & 0b0000_0010
+        button_L2 = rc[2] & 0b0010_0000
 
         return RobotObservation(
             joint_position=joint_position,  # in sim order, relative to q0
             joint_velocity=joint_velocity,  # in sim order
             gyroscope=gyroscope,
             quaternion=quaternion,
-            rpy=rpy,
+            roll=roll,
+            pitch=pitch,
+            yaw=yaw,
             lx=lx,
             ly=ly,
             rx=rx,
             ry=ry,
+            L1=bool(button_L1),
+            L2=bool(button_L2),
         )
 
     def set_act(self, action: torch.Tensor):